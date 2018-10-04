f34748a058ea56230cf6258b30bcbe7a132f2b63
Jose Martinez <sotero.valdez@gmail.com>
2018-10-03 21:55:36 -0700

    added something

--- CatApp/models.py
+++ CatApp/models.py
@@ -9,11 +9,17 @@ class PictureInfoModel(models.Model):
     class Meta:
         abstract = True
 
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
 class CatPicture(PictureInfoModel):
     breed = models.CharField(max_length=80, blank=True, default='unknown')
     cat_name = models.CharField(max_length=120)
+    photo = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
     def __str__(self):
         return self.cat_name
+
     def save(self, *args, **kwargs):
         """ on save, update timestamps """
         if not self.id:
